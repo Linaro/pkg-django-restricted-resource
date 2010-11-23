b626adbf9296a786e5235e1d02be5a4cea187e51
Zygmunt Krynicki <zygmunt.krynicki@linaro.org>
2010-11-24 00:27:24 +0100

    Fix bug in RestrictedResourceManager.owned_by_principal()

--- django_restricted_resource/managers.py
+++ django_restricted_resource/managers.py
@@ -52,8 +52,8 @@ class RestrictedResourceManager(models.Manager):
         instance.
         """
         if isinstance(principal, (User, AnonymousUser, type(None))):
-            user = filter_bogus_users(user)
-            return _owned_by_user(user)
+            user = filter_bogus_users(principal)
+            return self._owned_by_user(user)
         elif isinstance(principal, Group):
             group = principal
             return self._owned_by_group(group)
